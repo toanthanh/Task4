688c723788fb25de23c5eca5d7459907a35d7fab
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2016-11-25 10:21:27 +0200

    ready

--- js/main-a.js
+++ js/main-a.js
@@ -1,9 +1,19 @@
 // Create XMLHttpRequest object. Name it xhr.
+var xhr = new XMLHttpRequest();
 
 // Create function 'showImages' which
-// checks that loaded content is ready (readyState and status) and
-// then adds the loaded HTML content to <ul> element.
+var showImages = function() {
+    // checks that loaded content is ready (readyState and status) and
+    if (this.readyState == 4 && this.status == 200) {
+        // then adds the loaded HTML content to <ul> element.
+        document.querySelector('ul').innerHTML = this.responseText;
+    }
+
+}
 
 // Open XMLHttpRequest connection to load images.html, use get method.
+xhr.open("GET", "images.html", true);
 // When ready state changes, call showImages function.
+xhr.onreadystatechange = showImages;
 // Send XMLHttpRequest.
+xhr.send();
