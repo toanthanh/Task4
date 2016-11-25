6de5ceefb55e0740123da96f7c1093ac00a5db86
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2016-11-25 14:54:13 +0200

    ready

--- js/main-c.js
+++ js/main-c.js
@@ -1,27 +1,57 @@
-// Tee XMLHttpRequest-objekti. Anna sen nimeksi xhr
-
 // Create XMLHttpRequest object. Name it xhr.
-
+var xhr = new XMLHttpRequest();
 // Create function 'showImages' which
-// checks that loaded content is ready (readyState and status) and
-// then converts the JSON loaded from the server to JavaScript object.
-// Inside the fuction create an empty string 'output' and
-// create a loop which builds the following HTML from every image: 
-/*
-<li>
-    <figure>
-        <a href="img/original/filename.jpg"><img src="img/thumbs/filename.jpg"></a>
-        <figcaption>
-            <h3>Title</h3>
-        </figcaption>
-    </figure>
-</li>
-// Make the HTML elements by using createElement-method and
-// add the attributes by using setAttribute method or element.attribute syntax.
-// Nest the elements with appendChild-method.
-// Finally place the elements into <ul> element to print them to the HTML page
-// Function ends.
+var showImages = function() {
+    // checks that loaded content is ready (readyState and status) and
+    if (this.readyState == 4 && this.status == 200) {
+        // then converts the JSON loaded from the server to JavaScript object.
+        var arr = JSON.parse(this.responseText);
+        // Create a loop which builds the following HTML from every image:
+        for (var i = 0; i < arr.length; i++) {
+            /*
+            <li>
+                <figure>
+                    <a href="img/original/filename.jpg"><img src="img/thumbs/filename.jpg"></a>
+                    <figcaption>
+                        <h3>Title</h3>
+                    </figcaption>
+                </figure>
+            </li>
+            */
+            // Make the HTML elements by using createElement-method and
+            // add the attributes by using setAttribute method or element.attribute syntax.
+            // Nest the elements with appendChild-method.
+            var title = document.createTextNode(arr[i].mediaTitle);
+            var h3 = document.createElement('h3');
+            h3.appendChild(title);
 
-// Open XMLHttpRequest connection to load images.json, use get method.
+            var figcaption = document.createElement('figcaption');
+            figcaption.appendChild(h3);
+
+            var img = document.createElement('img');
+            img.setAttribute('src', 'img/thumbs/' + arr[i].mediaThumb)
+            var a = document.createElement('a');
+            a.setAttribute('href', 'img/original/' + arr[i].mediaUrl);
+            a.appendChild(img);
+
+            var figure = document.createElement('figure');
+            figure.appendChild(a);
+            figure.appendChild(figcaption);
+
+            var li = document.createElement('li');
+            li.appendChild(figure);
+
+            // Finally place the elements into <ul> element to print them to the HTML page
+            var ul = document.querySelector('ul');
+            ul.appendChild(li);
+        } // end for
+    } // end if
+    // Function ends.
+}
+
+// Open XMLHttpRequest connection to load images.html, use get method.
+xhr.open("GET", "images.json", true);
 // When ready state changes, call showImages function.
+xhr.onreadystatechange = showImages;
 // Send XMLHttpRequest.
+xhr.send();
