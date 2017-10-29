44625e939427905ed6b0bf17c82960cf4adc280a
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2017-10-29 17:54:15 +0200

    Update main-a.js

--- js/main-a.js
+++ js/main-a.js
@@ -1,9 +1,2 @@
-// Create XMLHttpRequest object. Name it xhr.
-
 // Create function 'showImages' which
-// checks that loaded content is ready (readyState and status) and
-// then adds the loaded HTML content to <ul> element.
-
-// Open XMLHttpRequest connection to load images.html, use get method.
-// When ready state changes, call showImages function.
-// Send XMLHttpRequest.
+// adds the loaded HTML content to <ul> element
