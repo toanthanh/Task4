24fb78a95424245930067209013c54d787f7dd34
toanthanh <thanhductoan1997@gmail.com>
2017-11-13 17:45:36 +0200

    Update main-a.js

--- js/main-a.js
+++ js/main-a.js
@@ -1,2 +1,14 @@
 // Create function 'showImages' which
 // adds the loaded HTML content to <ul> element
+const loadHTML = (query, url) => {
+    const element = document.querySelector(query);
+    fetch(url)
+        .then((response) => {
+        return response.text();
+    })
+        .then((text) => {
+        element.innerHTML = text;
+    });
+};
+
+loadHTML('ul', 'images.html');
